d6750d399d617ce8a9a8b55b970f6535ae934670
Roy <royf82092@gmail.com>
2022-06-27 19:13:18 +0700

    penambahan datebase tahun ajaran

--- database/migrations/2022_06_27_080947_create_tahun_ajaran.php
+++ database/migrations/2022_06_27_080947_create_tahun_ajaran.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('tahun_ajaran', function (Blueprint $table) {
             $table->id();
+            $table->string('tahun_ajaran');
             $table->timestamps();
         });
     }
