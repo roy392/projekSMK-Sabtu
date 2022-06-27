de52c3f496f0dbf3aa05f0bb5545f8fedde0e801
Roy <royf82092@gmail.com>
2022-06-27 19:17:16 +0700

    penambahan create datebase siswa

--- database/migrations/2022_06_27_080912_create_siswa.php
+++ database/migrations/2022_06_27_080912_create_siswa.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('siswa', function (Blueprint $table) {
             $table->id();
+            $table->string('nama_siswa');
+            $table->string('nis');
+            $table->string('jenis_kelamin');
+            $table->string('tempat_lahir');
+            $table->string('tanggal_lahir');
+            $table->string('no_hp');
+            $table->string('asal_sekolah');
+            $table->string('nama_ortu');
+            $table->string('pekerjaan_ortu');
             $table->timestamps();
         });
     }
