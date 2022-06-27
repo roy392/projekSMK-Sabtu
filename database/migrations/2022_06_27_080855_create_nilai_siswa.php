f67972798b5d17b2041712d2ef0849e2079dbd49
Roy <royf82092@gmail.com>
2022-06-27 19:25:31 +0700

    penambahan craeate datebase nilai siswa

--- database/migrations/2022_06_27_080855_create_nilai_siswa.php
+++ database/migrations/2022_06_27_080855_create_nilai_siswa.php
@@ -15,6 +15,19 @@ return new class extends Migration
     {
         Schema::create('nilai_siswa', function (Blueprint $table) {
             $table->id();
+            $table->string('id_nilai_siswa');
+            $table->string('nis');
+            $table->string('id_matpel');
+            $table->string('id_guru');
+            $table->string('id_tahun_ajaran');
+            $table->string('id_semester');
+            $table->decimal('nilai_harian1');
+            $table->decimal('nilai_harian2');
+            $table->decimal('nilai_harian3');
+            $table->decimal('nilai_tugas1');
+            $table->decimal('nilai_tugas2');
+            $table->decimal('nilai_uts');
+            $table->decimal('nilai_uas');
             $table->timestamps();
         });
     }
